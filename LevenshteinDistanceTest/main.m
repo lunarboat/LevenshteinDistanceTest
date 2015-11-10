35cb6eda432b43f63c236ade00895e2b0b5f593c
lunarboat <lunarboat@outlook.com>
2015-11-10 20:19:18 +0800

    Initial Commit

---
+++ LevenshteinDistanceTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LevenshteinDistanceTest
+//
+//  Created by lunarboat on 15/11/10.
+//  Copyright © 2015年 lunarboat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
